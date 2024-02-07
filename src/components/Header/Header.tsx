46c10e84b6f4f922b6a26044a20c8b8e37c897bc
LisaLapi <lisa.lapierre@oclock.school>
2024-02-07 16:38:15 +0100

    Header + navbar travail en cours

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Logo from '../../assets/Logo.png';
+import Avatar from '../../assets/Avatar.png';
+import './Header.scss';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
-      {/* 
-      
-      Un logo
-      Le titre du site (desktop)
-      Une icone utilisateur avec menu déroulant
-
-      Une barre de navigation (mobile : en bas, desktop : en haut) avec un champ de recherche
-      Récupérer l'url actuelle (hook useLocation) pour selected dans navbar 
-      Navlinks
-      
-      */}
-
-      <h1>Y'avait quoi avant ? </h1>
+      <div className="header">
+        {/* Logo avec lien vers la page d'accueil */}
+        <Link to="/">
+          <img alt="Logo" src={Logo} className="logo" />
+        </Link>
+        {/* Avatar d'utilisateur connecté avec menu déroulant */}
+        <div className="dropdown dropdown-end fixed right-2 top-2">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img alt="Avatar" src={Avatar} />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/profile">Mon profil</Link>
+            </li>
+            <li>
+              <Link to="/contributions">Mes contributions</Link>
+            </li>
+            <li>
+              <a>Déconnexion</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Navbar />
     </div>
-  )
+  );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,51 +1,24 @@
 import React from 'react';
 import './Navbar.scss';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-white">
-      <div className="flex-1 justify-center">
-        <a className="btn btn-ghost text-base">Accueil</a>
-        <a className="btn btn-ghost text-base">Voir les souvenirs</a>
-        <a className="btn btn-ghost text-base">Partager un souvenir</a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
+    <div>
+      {/* Barre de navigation version Desktop */}
+      <div className="navigation">
+        <div className="navbar-top justify-center">
+          <NavLink className="btn btn-ghost text-base" to="/">Accueil</NavLink>
+          <NavLink className="btn btn-ghost text-base" to="/memories">Voir les souvenirs</NavLink>
+          <NavLink className="btn btn-ghost text-base" to="/memories/create">Partager un souvenir</NavLink>
+        </div>
+        {/* Champs de recherche version desktop*/}
+        <div className='searchbar'>
           <input
             type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
+            placeholder="Rechercher..."
+            className="search-input"
           />
-        </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>
-                Mon profil
-              </a>
-            </li>
-            <li>
-              <a>Mes contributions</a>
-            </li>
-            <li>
-              <a>Déconnexion</a>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
