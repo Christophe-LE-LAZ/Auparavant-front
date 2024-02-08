d8dab6969d1e15b3c3536295f4f9da5798c7a5d1
Steven-nguyen03 <nguyen.stevenpro@gmail.com>
2024-02-08 23:00:00 +0100

    Slider finished and beggining of map

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Slider from '../Slider/Slider';
+import Map from '../Map/Map';
+import './Home.scss';
 
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
+    <div className=''>
+      <div className=''>
+        <h1 className='flex justify-center py-4' >O' Mais y'avait quoi avant</h1>
+        <p className='py-4 w-3/4 mx-auto text-center text-xs'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum harum iusto et dolorem tempore enim, placeat explicabo quis iure quae eligendi voluptate consequuntur cupiditate reiciendis magnam quaerat delectus qui magni minima veniam possimus repellendus. Perspiciatis accusantium repellat repellendus, quaerat odio dicta optio nostrum molestias voluptas iure doloribus dolor fugit ad!
+        </p>
+      </div>
+      <Slider />
+      <Map />
     </div>
+
   )
 }
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import './Map.scss';
+
+
+export default function Map() {
+  return (
+    <MapContainer center={[48.8566, 2.3522]} zoom={13} >
+        <TileLayer 
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+    </MapContainer>
+  );
+}
+
+
+
+
+
