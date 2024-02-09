cf29355ae4372b6a46ebcbadb12b7a210d0d5d76
Steven-nguyen03 <nguyen.stevenpro@gmail.com>
2024-02-09 11:18:25 +0100

    Responsive Title and description done

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,11 +6,11 @@ import './Home.scss';
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className='max-'>
       <div className=''>
-        <h1 className='flex justify-center py-3 text-lg' >O' Mais y'avait quoi avant</h1>
-        <p className='pb-3 w-3/4 mx-auto text-center text-sm lg:text-base '>
-        Nos villes évoluent, et par conséquent nos quartiers, nos lieux préférés également... Saurions nous dire ce qu'il y avait avant à tel endroit ? Probablement que non. Après avoir constaté cela, on s'interroge, existe-t-il des ressources sur le web nous permettant facilement d'accéder à la donnée ?
+        <h1 className='flex justify-center text-xl py-3 lg:text-3xl lg:py-14' >O' Mais y'avait quoi avant</h1>
+        <p className='pb-3 w-3/4 mx-auto text-center text-sm lg:text-base lg:leading-relaxed'>
+        Nos villes évoluent, et par conséquent nos quartiers, nos lieux préférés également... Saurions nous dire ce qu'il y avait avant à tel endroit ? Probablement que non. Après avoir constaté cela, on s'interroge, existe-t-il des ressources sur le web nous permettant facilement d'accéder à la donnée ? Wikipédia répond en partie à ça mais il faut souvent multiplier les recherches pour trouver les informations sur un lieu à différentes époques.
         </p>
       </div>
       <Slider />
