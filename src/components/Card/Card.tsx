37438131af8a2badd4522b98964f44a6964af2ad
Steven-nguyen03 <nguyen.stevenpro@gmail.com>
2024-02-09 12:02:51 +0100

    Responsive Card done

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,13 +3,13 @@ import { IMemory } from '../../types/memory';
 
 export default function Card() {
   return (
-    <div className=''>
-      <div className="card w-3/4 bg-red-100 shadow-xl mx-auto mb-10">
+    <div className='lg:flex flex-row'>
+      <div className="card w-3/4 bg-red-100 shadow-xl mx-auto mb-20 lg:mx-8">
   <figure className="px-10 pt-10">
     <img src="https://images.unsplash.com/photo-1523435324848-a7a613898152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80" alt="Shoes" className="rounded-xl" />
   </figure>
   <div className="card-body items-center text-center">
-    <h2 className="card-title text-xs">lorem </h2>
+    <h2 className="card-title text-xs lg:text-xl">lorem </h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda deserunt, aliquid sequi ut aperiam perferendis?</p>
     <div className="card-actions">
       <button className="btn btn-primary text-xs" >Voir +</button>
@@ -18,12 +18,12 @@ export default function Card() {
 </div>
 
 
-<div className="card w-3/4 bg-green-100 shadow-xl mx-auto mb-20">
+<div className="card w-3/4 bg-green-100 shadow-xl mx-auto mb-20 lg:mx-8">
   <figure className="px-10 pt-10">
     <img src="https://images.unsplash.com/photo-1523435324848-a7a613898152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80" alt="Shoes" className="rounded-xl" />
   </figure>
   <div className="card-body items-center text-center">
-    <h2 className="card-title text-xs">lorem </h2>
+    <h2 className="card-title text-xs lg:text-xl">lorem </h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda deserunt, aliquid sequi ut aperiam perferendis?</p>
     <div className="card-actions">
       <button className="btn btn-primary text-xs" >Voir +</button>
@@ -31,6 +31,19 @@ export default function Card() {
   </div>
 </div>
 
+
+<div className="card w-3/4 bg-purple-100 shadow-xl mx-auto mb-20 lg:mx-8">
+  <figure className="px-10 pt-10">
+    <img src="https://images.unsplash.com/photo-1523435324848-a7a613898152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80" alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-xs lg:text-xl ">lorem </h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda deserunt, aliquid sequi ut aperiam perferendis?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary text-xs" >Voir +</button>
+    </div>
+  </div>
+</div>
 
     </div>
   )
