adc5cc036bb36007260b86638f8017dd13517c15
LisaLapi <lisa.lapierre@oclock.school>
2024-02-12 17:22:07 +0100

    Login/Logout  WIP - conditional display in header - User reducer with untested AsyncThunk

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,35 +1,78 @@
-import React from 'react';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { changeEmail, changePassword, login } from '../../store/user';
 
 export default function Login() {
-  const { email, password } = useAppSelector((state) => state.user.credentials);
-  const pseudo = useAppSelector((state) => state.user.pseudo);
-  const loggedMessage = `Bienvenue ${pseudo}`;
-  const islogged = useAppSelector((state) => state.user.logged);
+  const dispatch = useAppDispatch();
+
+  const handleChangeEmail = (e) => {
+    dispatch(changeEmail(e.target.value));
+  };
+
+  const handleChangePassword = (e) => {
+    dispatch(changePassword(e.target.value));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login());
+  };
+
+  // Lecture des states du User reducer
+  const loading = useAppSelector((state) => state.user.loading);
+  const error = useAppSelector((state) => state.user.error);
+  const email = useAppSelector((state) => state.user.credentials.username);
+  const password = useAppSelector((state) => state.user.credentials.password);
 
   return (
     <div className="flex flex-col items-center m-10 gap-5 sm:m-20">
       <h2 className="text-xl">Connectez-vous</h2>
-      <label className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">E-mail</span>
+      <form onSubmit={handleSubmit}>
+        {/* Champ Email */}
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">E-mail</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Entrez votre email"
+            className="input input-bordered w-full max-w-xs"
+            onChange={handleChangeEmail}
+            value={email}
+          />
+        </label>
+        {/* Champ Password */}
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Mot de passe</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Entrez votre mot de passe"
+            className="input input-bordered w-full max-w-xs"
+            onChange={handleChangePassword}
+            value={password}
+          />
+        </label>
+        <div className="flex content-center mt-6">
+          {/* Bouton de soumission du formulaire */}
+          <button
+            type="submit"
+            className="h-12 rounded-lg p-3 bg-base-200 text-sm"
+          >
+            Soumettre
+          </button>
+          {/* Affichage d'un loader pendant le loading */}
+          {loading && (
+            <span className="loading loading-spinner loading-md ml-5"></span>
+          )}
         </div>
-        <input
-          type="text"
-          placeholder=""
-          className="input input-bordered w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Mot de passe</span>
+      </form>
+      {/* Affichage d'un message d'erreur */}
+      {error && (
+        <div role="alert" className="alert alert-error max-w-xs">
+          <span>{error}</span>
         </div>
-        <input
-          type="text"
-          placeholder=""
-          className="input input-bordered w-full max-w-xs"
-        />
-      </label>
+      )}
     </div>
   );
 }
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,21 +1,90 @@
-import {createReducer} from '@reduxjs/toolkit';
+import {createReducer, createAsyncThunk, createAction} from '@reduxjs/toolkit';
 import { ICredentials } from '../types/credentials'
+import axios from 'axios';
+import { RootState } from '.';
 
 interface UserState {
     logged: boolean;
+    id: number | null
+    firstname: string;
     credentials : ICredentials;
-    pseudo : string | null; 
+    loading: boolean;
+    error: null | string;
 }
 
 export const initialState: UserState = {
-    logged: false,
+    logged: true,
+    id: null,
+    firstname:'prénom',
     credentials: {
-        email: '',
+        username: '',
         password: '',
     },
-    pseudo : null,
+    loading: false,
+    error: null
 }
 
-const userReducer = createReducer(initialState, ()=>{});
+export const changeEmail = createAction<string>('user/changeEmail');
 
-export default userReducer;
+export const changePassword = createAction<string>('user/changePassword');
+
+export const login = createAsyncThunk('user/login', async (_, thunkAPI) => {
+    // Récupération de la valeur du state via la thunkAPI
+    const state = thunkAPI.getState() as RootState;
+    // Récupération des valeurs contenues dans credentials
+    const credentials = state.user.credentials as ICredentials;
+    // Envoi des credentials
+    const { data } = await axios.post('http://aurelien-rouchette-maret.vpnuser.lan/apotheosis/what-did-there-use-to-be-back/public/api/login_check', credentials);
+    console.log(data);
+    // Configuration de l'instance d'axios avec le token reçu
+    axios.defaults.headers.common = { Authorization: `Bearer ${data.token}` };
+    return data;
+  });
+  
+
+export const logout = createAction('user/logout');
+
+
+const userReducer = createReducer(initialState, (builder) => {
+    // Changement de la valeur de l'email dans les credentials
+    builder.addCase(changeEmail, (state, action) => {
+        state.credentials.username = action.payload;
+        console.log(state.credentials.username) 
+    })
+    // Changement de la valeur du password dans les credentials
+    .addCase(changePassword, (state, action) =>{ 
+        state.credentials.password = action.payload;
+        console.log(state.credentials.password)
+    })
+    // Gestion du cas "pending" de la connexion 
+    .addCase(login.pending, (state) => {
+        state.error = null;
+        state.loading = true;
+      })
+      // Gestion du cas "rejected" de la connexion
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message as string;
+      })
+      // Gestion du cas "fullfilled" de la connexion
+      .addCase(login.fulfilled, (state, action) => {
+        const { id, firstname } = action.payload;
+        state.loading = false;
+        state.id = id;
+        state.firstname = firstname;
+        state.logged = true;
+        state.credentials.username = '';
+        state.credentials.password = '';
+      })
+      // Gestion de la déconnexion
+      .addCase(logout, (state) => {
+        state.id = null
+        state.firstname = '';
+        state.logged = false;
+        // Suppression du token des headers de l'instance axios
+        axios.defaults.headers.common = {};
+      })
+  });
+  
+  export default userReducer;
+  
