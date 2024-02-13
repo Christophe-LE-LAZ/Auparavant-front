4fcbbe2ff4b759d5cfbe65be02176550231dd9b8
Steven-nguyen03 <nguyen.stevenpro@gmail.com>
2024-02-13 16:03:35 +0100

    work in progress

--- src/components/Memory/Memory.tsx
+++ src/components/Memory/Memory.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
-
+import NotFound from '../NotFound/NotFound';
 
 const MemoryPage = () => {
   const { id } = useParams<{ id: string }>();
-  const memoriesList = useAppSelector((state)=> state.memories.list);
-  const memory = memoriesList.find(memory => memory.memory_id.toString() === id);
+  const memoriesList = useAppSelector((state) => state.memories.list);
+  const memory = memoriesList.find(
+    (memory) => memory.memory_id.toString() === id
+  );
 
-  
+  if (!memory) {
+    return <NotFound />;
+  }
+
   return (
-    
-        <h2 className="text-sm lg:text-xl pt-4">{memory.title} </h2>
-        
+    <div className="text-center max-w-lg mx-auto">
+      <h1 className="text-sm lg:text-xl pt-4">{memory.title} </h1>
+      <p>Ajouté par xxx le {memory?.created_at}</p>
+      <p>Nom du lieu / type de lieu</p>
+      <img
+            src="https://images.unsplash.com/photo-1523435324848-a7a613898152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80"
+            alt=""
+            className="rounded-xl mx-auto"
+          />
+
+      <p>{memory.content}</p>
+      
+    </div>
   );
 };
 
