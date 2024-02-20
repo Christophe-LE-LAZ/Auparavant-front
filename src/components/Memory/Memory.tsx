de39b3843b6b36a598e8fc86f07eb9f35fd443e0
LisaLapi <lisa.lapierre@oclock.school>
2024-02-20 16:04:46 +0100

    Single memory page Update

--- src/components/Memory/Memory.tsx
+++ src/components/Memory/Memory.tsx
@@ -8,33 +8,84 @@ const MemoryPage = () => {
   const memoriesList = useAppSelector((state) => state.memories.list);
   const memory = memoriesList.find((memory) => memory.id.toString() === id);
 
-
   if (!memory) {
     return <NotFound />;
   }
 
-    // Convertir la date en objet Date
-    const memoryDate = new Date(memory.picture_date);
-    // Formater la date pour afficher sans les secondes
-    const formattedDate = memoryDate.toLocaleString(undefined, {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    });
-  
+  // Convertir la date en objet Date
+  const memoryDate = new Date(memory.picture_date);
+  // Formater la date pour afficher sans les secondes
+  const formattedDate = memoryDate.toLocaleString(undefined, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 
   return (
-    <div className="text-center max-w-lg mx-auto">
-      <h1 className="text-xl lg:text-3xl pt-10 pb-5">{memory.title} </h1>
-      <p>Ajouté par xxx le {formattedDate}</p>
-      <p className="pb-5">
-        {memory.place.name} / {memory.place.type}
-      </p>
-      <div className='mx-8'>
-        <img src={memory.main_picture} alt="" className="rounded-xl" />
+    <>
+      <Link to="/memories" className="link ml-5">
+        Retour à la liste des souvenirs
+      </Link>
+      <div className="max-w-lg mx-auto">
+        {/* Titre */}
+        <h1 className="text-center text-xl font-bold lg:text-2xl pt-10 pb-5">
+          {memory.title}{' '}
+        </h1>
+        {/* Image */}
+        <div className="mx-8">
+          <img src={memory.main_picture} alt="" className="rounded-xl" />
+        </div>
+        {/* Informations */}
+        <div className="flex flex-col mt-8">
+          {/* Partie "souvenir" */}
+          <legend className="text-lg font-bold">Le souvenir</legend>
+          <div className="px-5 py-2 border rounded-lg">
+            <div className="text-left mt-2">
+              <strong>Description :</strong> {memory.content}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Date de la photo :</strong> {formattedDate}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Ajouté par :</strong> {memory.user.firstname}{' '}
+              {memory.user.lastname}
+            </div>
+          </div>
+          {/* Partie "lieu" */}
+          <legend className="text-lg font-bold mt-5">Le lieu</legend>
+          <div className="px-5 py-2 border rounded-lg">
+            <div className="text-left mt-2">
+              <strong>Nom du lieu :</strong> {memory.place.name}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Type de lieu :</strong> {memory.place.type}
+            </div>
+          </div>
+          {/* Partie "localisation" */}
+          <legend className="text-lg font-bold mt-5">Sa localisation</legend>
+          <div className="px-5 py-2 border rounded-lg mb-10">
+            <div className="text-left mt-2">
+              <strong>Région :</strong> {memory.location.area}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Département :</strong> {memory.location.department}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Quartier :</strong> {memory.location.district}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Adresse :</strong> {memory.location.street}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Code postal :</strong> {memory.location.zipcode}
+            </div>
+            <div className="text-left mt-2">
+              <strong>Ville :</strong> {memory.location.city}
+            </div>
+          </div>
+        </div>
       </div>
-      <p className='text-sm px-10 mt-5 mb-5'>{memory.content}</p>
-    </div>
+    </>
   );
 };
 
