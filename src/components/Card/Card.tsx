ca878e4c7bf2806238645bfef57a290a554c0d12
LisaLapi <lisa.lapierre@oclock.school>
2024-02-23 13:22:36 +0100

    Responsive drawer on Memories list

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,8 +15,8 @@ export default function Card({ memory }: Props) {
 
   return (
     <Link to={`/memories/${memory.id}`}>
-      <div className="text-center max-w-lg mx-auto ">
-        <div className="card w-4/4 bg-gray-100 shadow-xl mx-auto mb-10 lg:mx-8">
+      <div className="text-center mx-auto ">
+        <div className="card bg-gray-100 shadow-xl mx-2 mb-10 w-96">
           <h2 className="text-lg font-bold lg:text-xl pt-4">{memory.title} </h2>
           <p></p>
           <h3 className="italic">
@@ -26,7 +26,7 @@ export default function Card({ memory }: Props) {
             <img
               src={memory.main_picture}
               alt=""
-              className="rounded-xl w-96 h-72"
+              className="rounded-xl w-80 h-72"
             />
           </figure>
           <div className="card-body items-center text-center "></div>
--- src/components/Memories/Memories.tsx
+++ src/components/Memories/Memories.tsx
@@ -5,14 +5,12 @@ import { clearMessage } from '../../store/messageReducer';
 import { fetchMemories } from '../../store/memoriesReducer';
 
 const Memories = () => {
-
   const dispatch = useAppDispatch();
 
   // Récupération des souvenirs depuis l'API
-    useEffect(() => {
-      dispatch(fetchMemories());
-    }, []);
-
+  useEffect(() => {
+    dispatch(fetchMemories());
+  }, []);
 
   // Récupération des valeurs du state
   const memoriesList = useAppSelector((state) => state.memories.list);
@@ -26,7 +24,7 @@ const Memories = () => {
     }
   }, []);
 
-  const handleOK= () => {
+  const handleOK = () => {
     setShowSuccess(false);
     dispatch(clearMessage());
   };
@@ -35,26 +33,64 @@ const Memories = () => {
     <div className="flex flex-col items-center">
       {/* Affichage d'un message si l'utilisateur vient juste de supprimer un souvenir */}
       {showSuccess && (
-        <div role="alert" className="flex alert alert-success text-sm max-w-sm justify-between mb-10">
+        <div
+          role="alert"
+          className="flex alert alert-success text-sm max-w-sm justify-between mb-10"
+        >
           <span>{message}</span>
           <button
-                className="text-sm bg-white font-bold py-1 px-2 rounded"
-                onClick={handleOK}
-              >
-                OK
-              </button>
+            className="text-sm bg-white font-bold py-1 px-2 rounded"
+            onClick={handleOK}
+          >
+            OK
+          </button>
         </div>
       )}
 
-      <h2 className="text-center text-2xl pb-10">Liste des souvenirs</h2>
       <div>
-        <ul className="flex flex-wrap">
-          {memoriesList.map((memory) => (
-            <li className="mx-auto" key={memory.id}>
-              <Card memory={memory} />
-            </li>
-          ))}
-        </ul>
+        {/* Responsive drawer avec filtres  */}
+
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col items-center justify-center">
+            {/* Page content here */}
+
+            <div className="flex justify-between items-center w-11/12 mb-5 lg:justify-center">
+              <div className='w-1/5 lg:hidden'></div>
+              <h2 className="w-3/5 text-center text-2xl">Liste des souvenirs</h2>
+              <label
+                htmlFor="my-drawer-2"
+                className="btn drawer-button w-1/5 lg:hidden"
+              >
+                Filtrer
+              </label>
+            </div>
+
+            <ul className="flex flex-wrap my-5">
+              {memoriesList.map((memory) => (
+                <li className="mx-auto" key={memory.id}>
+                  <Card memory={memory} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="drawer-side z-50">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              {/* Sidebar content here */}
+              <li>
+                <a>Sidebar Item 1</a>
+              </li>
+              <li>
+                <a>Sidebar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
