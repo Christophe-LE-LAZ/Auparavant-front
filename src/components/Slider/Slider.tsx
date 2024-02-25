cb1e0622672d48239845f65a5af536c5e28d55be
Steven-nguyen03 <nguyen.stevenpro@gmail.com>
2024-02-26 00:27:47 +0100

    SliderRandom done with new url

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -10,14 +10,18 @@ const Slider = () => {
 
   const randomPicture = useAppSelector((state) => state.randomMemoryReducer.picture);
 
+
+  const imageUrl1 = `https://admin.auparavant.fr/assets/pictures/${randomPicture.picture1}`;
+  const imageUrl2 = `https://admin.auparavant.fr/assets/pictures/${randomPicture.picture2}`;
+
   return (
     <div className='max-w-screen-xl mx-auto p-5'>
       <div className="diff aspect-[16/9]">
         <div className="diff-item-1">
-          <img className='m-auto' alt="test" src={randomPicture.picture1} />
+          <img className='m-auto' alt="test" src={imageUrl1} />
         </div>
         <div className="diff-item-2">
-          <img className='m-auto' alt="test" src={randomPicture.picture2} />
+          <img className='m-auto' alt="test" src={imageUrl2} />
         </div>
         <div className="diff-resizer"></div>
       </div>
